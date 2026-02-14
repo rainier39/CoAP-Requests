7c14be74a38f4f0ff6db4acc337b53788bbe9629
rainier39 <196280087+rainier39@users.noreply.github.com>
2026-02-13 23:38:59 -0800

    Minor update
    -Parse URI, support arbitrary port numbers
    -Make GET requests actually work
    -Send an actual messageID with each request

--- coap_requests.py
+++ coap_requests.py
@@ -1,28 +1,30 @@
 # https://github.com/rainier39/CoAP-Requests
 
 import socket
-
-# Default port for COAP.
-PORT = 5683
+import random
 
 # Main function handling the requests.
-def _rawCoAP(hostname: str, code: bytearray, payload: str):
+def _rawCoAP(hostname, code: bytearray, payload="", port: int=5683, options: bytearray=""):
   if (hostname.startswith("coap://")):
     hostname = hostname[7:]
 
   server = socket.gethostbyname(hostname)
 
   conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  conn.connect((server, PORT))
+  conn.connect((server, port))
 
   ver_t_tkl = b"\x40"
-  messageID = b"\x00\x00"
+  # We generate a pseudo-random messageID each time.
+  messageID = random.randbytes(2)
   # Padding is only sent if there is a payload.
   if (len(payload) > 0):
     padding = b"\xff"
   else:
     padding = b""
-  conn.send((ver_t_tkl + code + messageID + padding + payload.encode()))
+  # If the payload is a regular string, turn it into bytes.
+  if (type(payload) == str):
+    payload = payload.encode()
+  conn.send((ver_t_tkl + code + messageID + options + padding + payload))
 
   resp = conn.recv(4096)
 
@@ -31,8 +33,43 @@ def _rawCoAP(hostname: str, code: bytearray, payload: str):
   return resp
 
 # Wrapper function for performing GET requests.
-def get(hostname: str, payload: str=""):
-  return _rawCoAP(hostname, b"\x01", payload)
+def get(uri):
+  if (uri.startswith("coap://")):
+    uri = uri[7:]
+  elif (uri.startswith("coaps://")):
+    print("Unsupported protocol: coaps.")
+    return
+  # Break up the URI into the hostname and payload portion.
+  if "/" in uri:
+    hostname = uri[:uri.find("/")]
+    payload = uri[uri.find("/")+1:]
+  else:
+    hostname = uri
+    payload = ""
+  # Use the provided port if there is one.
+  if ":" in hostname:
+    port = int(hostname[hostname.find(":")+1:])
+    hostname = hostname[:hostname.find(":")]
+  # Otherwise assume the default coap port as per the coap specification.
+  else:
+    port = 5683
+  # If the payload is a regular string, turn it into bytes.
+  if (type(payload) == str):
+    payload = payload.encode()
+  options = b""
+  if (len(payload) > 0):
+    if (len(payload) < 13):
+      options = (int("b0", 16)^len(payload)).to_bytes()
+    elif (len(payload) < 269):
+      options = b"\xbd"
+      options += (len(payload)-13).to_bytes()
+    # Doesn't currently error out if the payload is larger than 16 bit int limit + 269. TODO
+    else:
+      options = b"\xbe"
+      options += (len(payload)-269).to_bytes()
+  if (options != ""):
+    options=options+payload
+  return _rawCoAP(hostname, b"\x01", "", port, options=options)
 
 # Send a GET request to a public test server.
-print(get("coap://coap.me"))
+print(get("coap://coap.me/test"))
